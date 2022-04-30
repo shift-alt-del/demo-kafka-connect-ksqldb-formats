009a8970b3504db2bf18846146fda262480cfec8
w-ding <wding@confluent.io>
2022-04-30 15:29:13 +0900

    Less demo data (4 -> 2)

--- mysql/add_data.sql
+++ mysql/add_data.sql
@@ -1,10 +1,6 @@
 INSERT INTO table_key_str (user_id, reason, duration_seconds) VALUES ("AAA", "purchase", 540);
 INSERT INTO table_key_str (user_id, reason, duration_seconds) VALUES ("BBB", "help", 224);
-INSERT INTO table_key_str (user_id, reason, duration_seconds) VALUES ("CCC", "help", 802);
-INSERT INTO table_key_str (user_id, reason, duration_seconds) VALUES ("DDD", "purchase", 10204);
 
 
 INSERT INTO table_key_int (user_id, reason, duration_seconds) VALUES (111, "purchase", 540);
 INSERT INTO table_key_int (user_id, reason, duration_seconds) VALUES (222, "help", 224);
-INSERT INTO table_key_int (user_id, reason, duration_seconds) VALUES (333, "help", 802);
-INSERT INTO table_key_int (user_id, reason, duration_seconds) VALUES (444, "purchase", 10204);
