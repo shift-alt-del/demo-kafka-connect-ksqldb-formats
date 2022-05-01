738cbaa6ddbbbb039b1c331cda4a346661c225d2
w-ding <wding@confluent.io>
2022-05-01 17:31:39 +0900

    Less data.

--- mysql/add_data.sql
+++ mysql/add_data.sql
@@ -1,6 +1,4 @@
 INSERT INTO table_key_str (user_id, reason, duration_seconds) VALUES ("AAA", "purchase", 540);
-INSERT INTO table_key_str (user_id, reason, duration_seconds) VALUES ("BBB", "help", 224);
 
 
 INSERT INTO table_key_int (user_id, reason, duration_seconds) VALUES (111, "purchase", 540);
-INSERT INTO table_key_int (user_id, reason, duration_seconds) VALUES (222, "help", 224);
